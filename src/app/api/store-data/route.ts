6836926231810cb390735e4f6be37474b870e6cb
Ken Lu <kenlu519@gmail.com>
2024-08-23 18:45:13 -0400

    Use node-cache to store scraped data

--- src/app/api/store-data/route.ts
+++ src/app/api/store-data/route.ts
@@ -1,10 +1,14 @@
 import { NextResponse } from 'next/server';
-let tempData: any = null;
+import NodeCache from 'node-cache';
+
+// Set up cache with TTL (time-to-live)
+const myCache = new NodeCache({ stdTTL: 600 }); // 10 minutes
 
 export async function POST(req: Request) {
   try {
     const data = await req.json();
-    tempData = data;
+    myCache.set("tempData", data); // Store data in the cache
+    console.log(myCache)
     return NextResponse.json({ message: 'Data stored successfully' }, {
       status: 200,
     });
@@ -14,9 +18,12 @@ export async function POST(req: Request) {
     });
   }
 }
+
 export async function GET() {
+  const tempData = myCache.get("tempData");
+  console.log(tempData)
   if (tempData) {
-    return NextResponse.json(tempData)
+    return NextResponse.json(tempData);
   } else {
     return NextResponse.json({ message: 'No data available' }, {
       status: 404,
